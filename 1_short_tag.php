ec3e746b5405ab55b114b5c64e69d00757d852c7
Shrikant Dhayje <shrikantdhayaje@gmail.com>
2022-03-27 17:51:27 +0530

    Update 1_short_tag.php

--- 1_short_tag.php
+++ 1_short_tag.php
@@ -1,7 +1,15 @@
 <html><body>
+
 <!-- Now this -->
 <?php echo "Hello World" ?>
+
 <!-- and This are same -->
 <?= "Hello World" ?>
+
+<!-- you can change this behavior under 
+php.ini configuration file where you write
+short_open_tag = false
+-->
+
 </body>
 </html>
