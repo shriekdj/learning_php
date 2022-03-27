da2e17adb378b9ebb501c9459d0a763f57919efa
Shrikant Dhayje <shrikantdhayaje@gmail.com>
2022-03-27 17:57:22 +0530

    Create 3_if_else_statements.php

---
+++ 3_if_else_statements.php
@@ -0,0 +1,8 @@
+<?php if ($expression == true): ?>
+This Will Be Shown
+
+<?php else: ?>
+Now This Thing is gonna shown instead of upper data
+
+<?php endif; ?>
+Don't forget endif have the semicolon at end
